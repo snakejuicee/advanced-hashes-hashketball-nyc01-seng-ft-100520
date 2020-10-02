29979490e162c10a1399d7dde829eb6b9a19dfad
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 10:53:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,8 +132,9 @@ end
 def num_points_scored(name)
   
   game_hash["away"].concat(game_hash["home"]).each do |p|
-    if p['player_name'] == name:
+    if p['player_name'] == name 
       return p['points']
+    end
   end 
 end
 
