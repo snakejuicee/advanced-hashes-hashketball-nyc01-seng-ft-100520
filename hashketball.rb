bdcfa8771f31541add8e090e2a1d9fd84ad5fb7c
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 10:49:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,48 @@ def game_hash
 end
 
 # Write code here
+
+
+def num_points_scored
+  
+end
+
+def shoe_size
+  
+end
+
+def team_colors
+  
+end
+
+def team_names
+  
+end
+
+def player_numbers
+  
+end
+
+def player_stats
+  
+end
+
+def big_shoe_rebounds
+  
+end
+
+def most_points_Scored
+  
+end
+
+def winning_team
+  
+end
+
+def player_with_longest_name
+  
+end
+
+def long_name_steals_a_ton?
+  
+end
